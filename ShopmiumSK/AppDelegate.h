37945e0edb8b3fa5b22a4afcba69bbb2b77ee05e
Salah Kaci <salah91@gmail.com>
2015-07-26 11:03:13 +0200

    Initial commit

---
+++ ShopmiumSK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopmiumSK
+//
+//  Created by Salah on 2015-07-26.
+//  Copyright (c) 2015 Salah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
