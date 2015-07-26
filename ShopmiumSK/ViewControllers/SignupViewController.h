38efac151abfa95ed80ea85c5e0eb6c8542b9209
Salah Kaci <salah91@gmail.com>
2015-07-26 12:28:07 +0200

    Set Signup view layout in Storyboard

---
+++ ShopmiumSK/ViewControllers/SignupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignupViewController.h
+//  ShopmiumSK
+//
+//  Created by Salah on 2015-07-26.
+//  Copyright (c) 2015 Salah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignupViewController : UIViewController
+
+@end
